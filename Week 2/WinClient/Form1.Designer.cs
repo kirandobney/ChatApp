a145a3cfc6743dbe84219694ae3dd5b662da050e
kirandobney <135818028+kirandobney@users.noreply.github.com>
2023-06-22 15:00:40 +0100

    Add files via upload

---
+++ Week 2/WinClient/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label = new System.Windows.Forms.Label();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.txtMessageHistory = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(13, 13);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(29, 13);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "Nick";
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(48, 10);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(100, 20);
+            this.txtNick.TabIndex = 1;
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.Location = new System.Drawing.Point(154, 8);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(75, 23);
+            this.btnSignIn.TabIndex = 2;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            // 
+            // txtMessageHistory
+            // 
+            this.txtMessageHistory.Location = new System.Drawing.Point(16, 37);
+            this.txtMessageHistory.Multiline = true;
+            this.txtMessageHistory.Name = "txtMessageHistory";
+            this.txtMessageHistory.ReadOnly = true;
+            this.txtMessageHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessageHistory.Size = new System.Drawing.Size(213, 261);
+            this.txtMessageHistory.TabIndex = 3;
+            this.txtMessageHistory.TextChanged += new System.EventHandler(this.txtMessageHistory_TextChanged);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(16, 304);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(132, 20);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(154, 304);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 339);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtMessageHistory);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.Label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.TextBox txtMessageHistory;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
